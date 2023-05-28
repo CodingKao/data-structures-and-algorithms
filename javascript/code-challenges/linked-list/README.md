afc1f8fcd42806393227c6e7e53b238098270fe2
Kao Saelor <saelor.kao@gmail.com>
2023-05-27 18:53:32 -0700

    redo zip challenge and re-organize challenge 5-8

--- javascript/code-challenges/linked-list/README.md
+++ javascript/code-challenges/linked-list/README.md
@@ -32,13 +32,13 @@ A Linked List is a sequence of Nodes that are connected/linked to each other. Th
       - "{ a } -> { b } -> { c } -> NULL"
 
 ### Whiteboard
-
+Not needed.
 
 ### Approach & Efficiency
 I created a Linked List class with a head property. I used a Node class to store values and a pointer to the next Node. I also created a separate linked-list-test.js to test the functionality of the Linked List class and ensure it behaves as expected.
 
 ### Solution
-![Class 5 Test](./assets/linked-list-test1.png)
+![Class 5 Solution](./assets/linked-list-test1-solution.png)
 
 ## Challenge 06: Linked List Insertion
 Extend a Linked List to allow various insertion methods.
@@ -66,7 +66,7 @@ Collaborated with Tricia Sawyer to do whiteboard.
 I approached the code challenge by traversing the linked list to locate the desired node for insertion. I then created a new node with the given value and linked it accordingly. The time complexity for these methods is O(n), as we need to iterate through the list. The space complexity is O(1), as we only create a constant number of new nodes.
 
 ### Solution
-
+![Class Solution 6](./assets/linked-insert-solution.png)
 
 ## Challenge 07: kth-from-end
 Create `kthFromTheEnd` method that works with Linked List.
@@ -84,21 +84,33 @@ Write the following method for the Linked List class:
 ![Class 7 Whiteboard](./assets/code-challenge-7.png)
 
 ### Approach & Efficiency
-
+I employed a two-pointer approach to find the k-th node from the end of the linked list. It has a time complexity of O(n) and a space complexity of O(1).
 
 ### Solution
-
+![Class 7 Solution](./assets/linked-list-kth-solution.png)
 
 ## Challenge 08: Zip
-Create `zipLists` function that works with Linked List.
+Write a function called ZipLists that takes two lists and returns a reference to the zipped list which has values alternating between each of the lists.
+
+**Feature Tasks**
+
+- Write a function called zip lists
+- Arguments: 2 linked lists
+- Return: New Linked List, zipped as noted below
+- Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the the zipped list.
+- Try and keep additional space down to O(1)
+- You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
 
 ### Whiteboard
+![Class 8 Whiteboard](./assets/code-challenge-8.png)
+
+Collaborated with Ethan Storm to do whiteboard.
 
 ### Approach & Efficiency
-
+To zip two linked lists together alternating their nodes, I used an iterative approach. I initialized two pointers to track the current nodes of each list and manipulated their links to create the zipped list. The time complexity is O(n), and the space complexity is O(1).
 
 ### Solution
-
+![Class 8 Solution](./assets/linked-list-zip-solution.png)
 
 ## Challenge 09: IsPalindrome
 Create `zipLists` function that works with Linked List.
---
+++ javascript/code-challenges/linked-list/linked-list-zip.test.js
@@ -0,0 +1,242 @@
+'use strict';
+
+const { LinkedList, zipLists } = require('./index-zip');
+
+describe('Linked List', () => {
+
+  test('Can successfully instantiate an empty linked list', () => {
+    const list = new LinkedList();
+
+    expect(list.head).toBeNull();
+  });
+
+  test('Can properly insert into the linked list', () => {
+    const list = new LinkedList();
+    list.insert(1);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next).toBeNull();
+  });
+
+  test('The head property will properly point to the first node in the linked list', () => {
+    const list = new LinkedList();
+    list.insert(2);
+
+    expect(list.head.value).toEqual(2);
+    expect(list.head.next).toBeNull();
+
+    list.insert(1);
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next).toBeNull();
+    list.append('a');
+    expect(list.head.next.next.value).toEqual('a');
+  });
+
+  test('Can properly insert multiple nodes into the linked list', () => {
+    const list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+
+    expect(list.head.value).toEqual(3);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(1);
+    expect(list.head.next.next.next).toBeNull();
+  });
+
+  test('Can successfully add a node to the end of the linked list', () => {
+    const list = new LinkedList();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.next.next.value).toEqual(3);
+    expect(list.head.next.next.next).toBeNull();
+  });
+
+  test('Will return true when finding a value within the linked list that exists', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(list.includes('b')).toEqual(true);
+  });
+
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(list.includes('g')).toEqual(false);
+  });
+
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+    list.append('l');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { l } -> NULL');
+  });
+
+  test('Can successfully insert a node before a node located in the middle of a linked list', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    list.insertBefore('c', 'l');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { l } -> { c } -> { d } -> NULL');
+  });
+
+  test('Can successfully insert after a node in the middle of the linked list', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    list.insertAfter('c', 'l');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { l } -> { d } -> NULL');
+  });
+
+  test('Can successfully insert a node after the last node of the linked list', () => {
+    const list = new LinkedList();
+    list.insert('d');
+    list.insert('c');
+    list.insert('b');
+    list.insert('a');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    list.insertAfter('d', 'l');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { l } -> NULL');
+  });
+
+  test('Where k is greater than the length of the linked list', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(list.kthFromEnd(10)).toEqual(null);
+  });
+
+  test('Where k and the length of the list are the same', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(list.kthFromEnd(3)).toEqual('a');
+  });
+
+  test('Where k is not a positive integer', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(list.kthFromEnd(-3)).toEqual('d');
+  });
+
+  test('Where the linked list is of a size 1', () => {
+    const list = new LinkedList();
+    list.append('a');
+
+    expect(list.toString()).toEqual('{ a } -> NULL');
+    expect(list.kthFromEnd(0)).toEqual('a');
+  });
+
+  test('“Happy Path” where k is not at the end, but somewhere in the middle of the linked list', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(list.kthFromEnd(2)).toEqual('b');
+  });
+
+  test('able to zip two lists together', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+    const list2 = new LinkedList();
+    list2.append('a');
+    list2.append('b');
+    list2.append('c');
+    list2.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(list2.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(zipLists(list, list2).toString()).toEqual('{ a } -> { a } -> { b } -> { b } -> { c } -> { c } -> { d } -> { d } -> NULL');
+  });
+
+  test('able to zip two lists together with list1 longer', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+    list.append('e');
+    const list2 = new LinkedList();
+    list2.append('a');
+    list2.append('b');
+    list2.append('c');
+    list2.append('d');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { e } -> NULL');
+    expect(list2.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(zipLists(list, list2).toString()).toEqual('{ a } -> { a } -> { b } -> { b } -> { c } -> { c } -> { d } -> { d } -> { e } -> NULL');
+  });
+
+  test('able to zip two lists together where list2 is longer', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+    const list2 = new LinkedList();
+    list2.append('a');
+    list2.append('b');
+    list2.append('c');
+    list2.append('d');
+    list2.append('e');
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(list2.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { e } -> NULL');
+    expect(zipLists(list, list2).toString()).toEqual('{ a } -> { a } -> { b } -> { b } -> { c } -> { c } -> { d } -> { d } -> { e } -> NULL');
+  });
+
+  test('test zip if one list is empty', () => {
+    const list = new LinkedList();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+    list.append('d');
+    const list2 = new LinkedList();
+
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+    expect(zipLists(list, list2).toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> NULL');
+  });
+
+});
